bf2dba2640df471c2daba4d4d1589015df5c73e4
David Anderson <dave@natulte.net>
2020-03-14 21:49:51 -0700

    Remove useDHCP directives, networkmanager now running the network.

--- vega/networking.nix
+++ vega/networking.nix
@@ -4,7 +4,4 @@
   networking.networkmanager.wifi.backend = "iwd";
   programs.nm-applet.enable = true;
   networking.useDHCP = false;
-  networking.interfaces.enp3s0f0.useDHCP = true;
-  networking.interfaces.enp5s0.useDHCP = true;
-  networking.interfaces.wlp1s0.useDHCP = true;
 }
