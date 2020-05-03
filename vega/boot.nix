34b4e05a48fb81b43fac197c1cd4144f6bf05fda
David Anderson <dave@natulte.net>
2020-05-03 00:07:34 +0000

    vega: prompt for ZFS keys.

--- vega/boot.nix
+++ vega/boot.nix
@@ -2,6 +2,7 @@
   boot = {
     kernelPackages = pkgs.linuxPackages_5_4;
     supportedFilesystems = ["zfs"];
+    zfs.requestEncryptionCredentials = true;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
