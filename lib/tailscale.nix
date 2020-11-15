cb8afe3ef041afe525f44fca1f709c0016890f18
David Anderson <dave@natulte.net>
2020-11-15 15:03:23 -0800

    Fix up tailscaled definitions.

--- lib/tailscale.nix
+++ lib/tailscale.nix
@@ -5,11 +5,15 @@
       default = false;
     };
   };
-  config = {
+  config = lib.mkIf (!config.boot.isContainer) {
     nixpkgs.overlays = [(final: prev: {
       tailscale = flakes.nixos-unstable.legacyPackages.x86_64-linux.tailscale;
     })];
     services.tailscale.enable = !config.my.disable-system-tailscale;
     environment.systemPackages = lib.mkIf (!config.my.disable-system-tailscale) [ pkgs.tailscale ];
+    systemd.services.tailscale.path = [ pkgs.openresolv ];
+    # scudo memory allocator segfaults unstable binaries for some
+    # reason. https://github.com/NixOS/nixpkgs/issues/100799
+    environment.memoryAllocator.provider = "libc";
   };
 }
