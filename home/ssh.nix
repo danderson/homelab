f6333acc9460b90642980dd528ed50cb2108d836
David Anderson <dave@natulte.net>
2020-11-15 01:25:00 -0800

    Enable SSH config management, add host blocks.

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,8 +1,28 @@
 {
   programs.ssh = {
-    enable = false;
+    enable = true;
     # TODO: moar
     controlMaster = "auto";
     controlPersist = "10m";
+
+    matchBlocks = {
+      acrux = {
+        host = "acrux acrux.universe.tf";
+        hostname = "acrux.universe.tf";
+        port = 42222;
+      };
+
+      atlas = {
+        host = "atlas atlas.universe.tf";
+        hostname = "atlas.universe.tf";
+        port = 42222;
+      };
+
+      vega = {
+        host = "vega vega.universe.tf";
+        hostname = "localhost"; # TODO: make this less silly.
+        port = 42222;
+      };
+    };
   };
 }
