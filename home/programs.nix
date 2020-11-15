32f9c01b56b97909bd32f774c71e948fe24a6a0b
David Anderson <dave@natulte.net>
2020-11-14 23:36:01 -0800

    Add neofetch and reorg programs.

--- home/programs.nix
+++ home/programs.nix
@@ -1,53 +1,54 @@
 { config, pkgs, lib, ... }:
+let
+  cli-programs = with pkgs; [
+    bc
+    conntrack-tools
+    dmidecode
+    dstat
+    efibootmgr
+    efivar
+    file
+    fping
+    git-crypt
+    go
+    ipcalc
+    ipmitool
+    jq
+    lftp
+    lm_sensors
+    lsof
+    mosh
+    neofetch
+    nix-diff
+    nmap
+    pciutils
+    psmisc
+    pwgen
+    rename
+    screen
+    smartmontools
+    sysstat
+    tcpdump
+    wget
+  ];
+  gui-programs = with pkgs; [
+    feh
+    gnome3.dconf-editor
+    gimp
+    google-chrome
+    graphviz
+    pavucontrol
+    pdftk
+    steam
+  ];
+in
 {
   options = {
     my.gui-programs = lib.mkEnableOption "GUI programs";
   };
 
-  config = let
-    gui = if config.my.gui-programs
-          then with pkgs; [
-            feh
-            gnome3.dconf-editor
-            gimp
-            google-chrome
-            graphviz
-            pavucontrol
-            pdftk
-            steam
-          ]
-          else [];
-  in {
-    home.packages = with pkgs; [
-      bc
-      conntrack-tools
-      dmidecode
-      dstat
-      efibootmgr
-      efivar
-      file
-      fping
-      git-crypt
-      go
-      ipcalc
-      ipmitool
-      jq
-      lftp
-      lm_sensors
-      lsof
-      mosh
-      nix-diff
-      nmap
-      pciutils
-      psmisc
-      pwgen
-      rename
-      screen
-      smartmontools
-      sysstat
-      tcpdump
-      wget
-    ] ++ gui;
+  config = {
+    home.packages = cli-programs ++ (if config.options.my.gui-programs then gui-programs else []);
     programs.lesspipe.enable = true;
     programs.dircolors.enable = true;
   };
