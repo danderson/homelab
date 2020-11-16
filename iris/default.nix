ce2f3429902f9c4f1f06556a15e8afea7259d430
David Anderson <dave@natulte.net>
2020-11-15 20:13:00 -0800

    iris: bring back into the fold.

--- iris/default.nix
+++ iris/default.nix
@@ -8,8 +8,6 @@ in
 
     ./private.nix
     ./hardware-configuration.nix
-    ./telegraf.nix
-    ./tailscale.nix
   ];
 
   my.cpu-vendor = "intel";
@@ -26,7 +24,6 @@ in
   networking = {
     hostName = "iris";
     hostId = "2a939c2d";
-    #defaultGateway = "192.168.17.1";
     nameservers = [ "8.8.8.8" ];
 
     interfaces.eno1.ipv4.addresses = [{
@@ -39,7 +36,7 @@ in
       enable = true;
     };
   };
-  
+
   powerManagement.cpuFreqGovernor = "performance";
 
   # List packages installed in system profile. To search, run:
@@ -49,14 +46,6 @@ in
       hdparm
       lsscsi
     ];
-  };
-
-  programs = {
-    iotop.enable = true;
-    iftop.enable = true;
-    mosh.enable = true;
-    mtr.enable = true;
-    zsh.enable = true;
   };
 
   services = {
@@ -71,59 +60,7 @@ in
       };
     };
     openssh.ports = [22];
-    fwupd.enable = false; # Causes mass build of stuff.
   };
-
-  # power.ups.enable = true
-
-  networking.firewall.allowedTCPPorts = [3000];
-  # containers = {
-  #   monitoring = {
-  #     autoStart = false;
-  #     bindMounts = {
-  #       "/data/monitoring" = {
-  #         hostPath = "/data/monitoring";
-  #         isReadOnly = false;
-  #       };
-  #     };
-  #     config = {
-  #       imports = [
-  #         ../lib
-  #       ];
-
-  #       services = {
-  #         prometheus = {
-  #           enable = true;
-  #           globalConfig = {
-  #             scrape_interval = "1s";
-  #             scrape_timeout = "1s";
-  #             evaluation_interval = "1s";
-  #           };
-  #           scrapeConfigs = [{
-  #             job_name = "localhost";
-  #             static_configs = [{
-  #               targets = ["localhost:9273"];
-  #               labels = { instance = "localhost"; };
-  #             }];
-  #           }];
-  #         };
-  #         grafana = {
-  #           enable = true;
-  #           addr = "";
-  #           provision = {
-  #             enable = true;
-  #             datasources = [{
-  #               name = "prometheus";
-  #               type = "prometheus";
-  #               url = "http://localhost:9090";
-  #               isDefault = true;
-  #             }];
-  #           };
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
