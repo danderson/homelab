6e1439641b656cbbccae2517568cf97ba33ad870
David Anderson <dave@natulte.net>
2020-11-17 00:19:42 -0800

    Remove ssh overrides.

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,25 +4,5 @@
     # TODO: moar
     controlMaster = "auto";
     controlPersist = "10m";
-
-    matchBlocks = {
-      acrux = {
-        host = "acrux acrux.universe.tf";
-        hostname = "192.168.1.2";
-        port = 42222;
-      };
-
-      atlas = {
-        host = "atlas atlas.universe.tf";
-        hostname = "atlas.universe.tf";
-        port = 42222;
-      };
-
-      vega = {
-        host = "vega vega.universe.tf";
-        hostname = "localhost"; # TODO: make this less silly.
-        port = 42222;
-      };
-    };
   };
 }
