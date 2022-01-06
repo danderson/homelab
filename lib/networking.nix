8589316debf26d3c7350d5dfa1e8c761ec0e85a1
David Anderson <dave@natulte.net>
2022-01-05 23:29:05 -0800

    Disable temporary addresses, don't use EUI64 IPv6 addrs.

--- lib/networking.nix
+++ lib/networking.nix
@@ -3,5 +3,10 @@
     useDHCP = false;
     domain = "universe.tf";
     iproute2.enable = true; # Copy iproute2 config files to /etc
+    tempAddresses = "disabled";
+  };
+  boot.kernel.sysctl = {
+    # Generate stable, but per-boot random SLAAC addresses, don't use EUI64.
+    "net.ipv6.conf.all.addr_gen_mode" = 3;
   };
 }
