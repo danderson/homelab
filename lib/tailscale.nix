9c844e66133b45087ef7144e1e795126ba33374c
David Anderson <dave@natulte.net>
2022-05-05 19:06:08 -0700

    lib/tailscale.nix: import some PRs until they hit unstable.

--- lib/tailscale.nix
+++ lib/tailscale.nix
@@ -6,15 +6,15 @@ let
 in
 {
   config = lib.mkIf config.my.tailscale {
+    # https://github.com/NixOS/nixpkgs/pull/170851
     warnings = lib.optional (firewallOn && rpfIsStrict) "Strict reverse path filtering breaks Tailscale exit node use and some subnet routing setups. Consider setting `networking.firewall.checkReversePath` = 'loose'";
 
     services.tailscale.enable = true;
     services.tailscale.package = flakes.nixos-unstable.legacyPackages.x86_64-linux.tailscale;
     networking.firewall.allowedUDPPorts = [ 41641 ];
-    systemd.services.tailscaled.path = [ pkgs.openresolv ];
-    # scudo memory allocator segfaults unstable binaries for some
-    # reason. https://github.com/NixOS/nixpkgs/issues/100799
-    environment.memoryAllocator.provider = "libc";
+    # https://github.com/NixOS/nixpkgs/pull/171747
+    systemd.services.tailscaled.path = [ pkgs.glibc ];
+    # https://github.com/NixOS/nixpkgs/pull/170210
     systemd.services.tailscaled.stopIfChanged = false;
   };
 }
