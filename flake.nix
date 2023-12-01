d974e18bd0ef9a4c56c968bec8aa84716472b0fc
David Anderson <dave@natulte.net>
2023-11-30 18:37:39 -0800

    prep for 23.11 update

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
   description = "Personal NixOS configs";
 
   inputs = {
-    nixos-old.url = github:NixOS/nixpkgs/nixos-22.11;
-    nixos.url = github:NixOS/nixpkgs/nixos-23.05;
+    nixos-old.url = github:NixOS/nixpkgs/nixos-23.05;
+    nixos.url = github:NixOS/nixpkgs/nixos-23.11;
     nixos-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
     nixos-dave.url = github:danderson/nixpkgs/danderson/influx2.4;
     nixos-unstable-small.url = github:NixOS/nixpkgs/nixos-unstable-small;
     nixos-hardware.url = github:NixOS/nixos-hardware;
     home-manager-old = {
-      url = github:nix-community/home-manager/release-22.11;
+      url = github:nix-community/home-manager/release-23.05;
       inputs.nixpkgs.follows = "nixos-old";
     };
     home-manager = {
-      url = github:nix-community/home-manager/release-23.05;
+      url = github:nix-community/home-manager/release-23.11;
       inputs.nixpkgs.follows = "nixos";
     };
     home-manager-unstable = {
@@ -79,14 +79,14 @@
       };
     in {
       nixosConfigurations = {
-        acrux = box nixos home-manager "acrux";
-        gacrux = box nixos home-manager "gacrux";
-        izar = box nixos home-manager "izar";
-        iris = box nixos home-manager "iris";
-        vega = box nixos home-manager "vega";
-        rigel = box nixos home-manager "rigel";
-        canopus = box nixos home-manager "canopus";
-        betelgeuse = box nixos home-manager "betelgeuse";
+        acrux = box nixos-old home-manager-old "acrux";
+        gacrux = box nixos-old home-manager-old "gacrux";
+        izar = box nixos-old home-manager-old "izar";
+        iris = box nixos-old home-manager-old "iris";
+        vega = box nixos-old home-manager-old "vega";
+        rigel = box nixos-old home-manager-old "rigel";
+        canopus = box nixos-old home-manager-old "canopus";
+        betelgeuse = box nixos-old home-manager-old "betelgeuse";
       };
 
       devShell.x86_64-linux = with nixos.legacyPackages.x86_64-linux; mkShell {
